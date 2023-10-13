642e9ad8d6fc2b8911bf0fca0d66085078ce45cd
Vedant Pandey <vedantpandey459@gmail.com>
2023-10-13 17:53:26 +0530

    flask done

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,15 +1,10 @@
 import React ,{useState} from "react";
 import UserNavigation from "../components/UserNavigation";
-import Navigation from "../components/Navigation";
-import { useLocation, Link, NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function Dashboard(){
-    const location = useLocation();
-    console.log(location)
-    let user = location.state.user;
-
     const [images,setimages]=useState([])
+    const [data,setdata]=useState(null)
     const handleImageInput=(event)=>{
         const file=event.target.files[0]
         setimages([...images,file])
@@ -21,18 +16,19 @@ export default function Dashboard(){
         formdata.append('image',images[0])
         console.log(formdata)
         const senddata=await axios.post(' http://127.0.0.1:5000/plantdetection/',formdata, {
+            withCredentials: true,
             headers: {
-              'Content-Type': 'multipart/form-data',
-
-            }})
-        const sendata= await senddata.json();
-        console.log(senddata)
-        window.location.reload()
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Access-Control-Allow-Origin': '*', 
+            },})
+        setdata(senddata.data)
     }
+    useEffect(()=>{
+        console.log(data)
+    })
     return (
         <div>
         <UserNavigation/>
-        <Navigation user={user} />
             <div style={{display:'flex',justifyContent:'center',marginTop:'200px',gap:20}}>
             <form onSubmit={handleSubmit}>
             <p style={{backgroundColor:'black',color:'white',paddingBlock:20,paddingInline:30 ,borderRadius:10,marginBottom:20}} >Upload Image in Choose file button below</p>
